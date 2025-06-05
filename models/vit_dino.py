7e1fd89a8f4c8f84d5392376e4eb71c3fa79f172
matteo-petrelli <mpetrelli99@gmail.com>
2025-06-05 19:29:15 +0200

    Create vit_dino.py

---
+++ models/vit_dino.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+import timm
+
+def get_dino_vit_s16(num_classes: int = 100):
+    """
+    Loads the DINO ViT-S/16 pretrained model and adapts it for CIFAR-100 classification.
+    
+    Args:
+        num_classes (int): Number of output classes for classification.
+    
+    Returns:
+        torch.nn.Module: Modified DINO ViT-S/16 model.
+    """
+    model = timm.create_model('vit_small_patch16_224_dino', pretrained=True)
+    
+    # Replace the classification head with one for CIFAR-100
+    model.head = nn.Linear(model.head.in_features, num_classes)
+    return model
