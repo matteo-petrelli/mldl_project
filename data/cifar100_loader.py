db9b5ff71e8e198d8967b62a39bdcbaf8b65204f
matteo-petrelli <mpetrelli99@gmail.com>
2025-06-12 17:04:29 +0200

    Update cifar100_loader.py

--- data/cifar100_loader.py
+++ data/cifar100_loader.py
@@ -7,12 +7,13 @@ import random
 
 def get_transforms():
     train_transform = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
+        transforms.Resize(224),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761))
     ])
     test_transform = transforms.Compose([
+        transforms.Resize(224),
         transforms.ToTensor(),
         transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761))
     ])
